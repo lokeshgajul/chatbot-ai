3c164bff559c2513b33976d81f66e8619114ea43
Fihd123 <sangeetagajul@gmail.com>
2024-08-14 22:50:02 +0530

    chatbot integrated

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Chat from "./components/chat";
+function App() {
+  return (
+    <>
+      <Chat />
+    </>
+  );
+}
+
+export default App;
